84724fe237561d4162dc8d3dfa9521ed4f78baa1
Christina Sohn <chsohn15@gmail.com>
2020-06-24 01:13:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,8 +26,11 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card + deal_card =card_total
+  card1 = deal_card
+  card2 = deal_card 
+  card_total = card1 + card2
   display_card_total(card_total)
+  card_total
 end
 
 def hit?
